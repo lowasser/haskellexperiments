7c3d6588b3415fa8ddae9fe71b3b95e51a3bb8ec
Louis Wasserman <wasserman.louis@gmail.com>
2012-07-14 20:49:33 +0200

    Update master

--- hello-world/helloworld.hs
+++ hello-world/helloworld.hs
@@ -1,10 +1,9 @@
-
 module Main where
 import Data.Char
 
 main = 	many [ "Hello World" ,"Another line?"]
 
-many = mapM_ putStrLn . upper
+many = mapM_ (putStrLn . upper)
 
 upper :: String -> String
 upper = map toUpper
